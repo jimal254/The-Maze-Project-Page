5a8622817295cda596dcce02d48b6b78d744ddad
jimmy maina <jimmymwas@gmail.com>
2024-09-12 12:39:50 -0700

    Initial commit

--- JS/MoreText.js
+++ JS/MoreText.js
@@ -1,15 +1,15 @@
 function myFunction() {
-    let dots = document.getElementById("dots");
-    let moreText = document.getElementById("more");
-    let btnText = document.getElementById("myBtn");
-  
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
     if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerText = "Read more"; 
-      moreText.style.display = "none";
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
     } else {
-      dots.style.display = "none";
-      btnText.innerText = "Read less"; 
-      moreText.style.display = "inline";
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
     }
 }
